60f555ce14e24b79d7cdfbedd49dd42e37bea97e
Anastasiya <anastasiya.ibr@mail.ru>
2023-11-08 22:48:47 +0300

    without using JSX

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
-//На всех строчка используется декларативный стиль, тк нет описания шагов получения результата, а только описание необходимого результата.
 import logo from "./logo.svg";
 import "./App.css";
 import { nowYear } from "./index";
+import { createElement } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div className="nowYear">{nowYear}</div>
-      </header>
-    </div>
+  return createElement(
+    "div",
+    { className: "App" },
+    "",
+    createElement(
+      "header",
+      { className: "App-header" },
+      "",
+      createElement("img", { src: logo, className: "App-logo", alt: "logo" }),
+      createElement(
+        "p",
+        null,
+        "Edit ",
+        createElement("code", null, "src/App.js"),
+        "and save to reload.",
+      ),
+      createElement(
+        "a",
+        {
+          className: "App-link",
+          href: "https://reactjs.org",
+          target: "_blank",
+          rel: "noopener noreferrer",
+        },
+        "Learn React",
+      ),
+      createElement("div", { className: "nowYear" }, nowYear),
+    ),
   );
 }
-
 export default App;
