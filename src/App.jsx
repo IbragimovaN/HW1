cc63c79728fcdfd34929d10bcba240cddf9e5771
Anastasiya <anastasiya.ibr@mail.ru>
2023-11-16 23:13:51 +0300

    задание 1 без доп.

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,91 @@
-import logo from "./logo.svg";
-import "./App.css";
-
+import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const btnNumbers = [
+    { id: "001", name: 1 },
+    { id: "002", name: 2 },
+    { id: "003", name: 3 },
+    { id: "004", name: 4 },
+    { id: "005", name: 5 },
+    { id: "006", name: 6 },
+    { id: "007", name: 7 },
+    { id: "008", name: 8 },
+    { id: "009", name: 9 },
+    { id: "010", name: 0 },
+  ];
+
+  // const [valueDisplay, setValueDisplay] = useState("");
+  const [currentNumberFirst, setCurrentNumberFirst] = useState("");
+  const [currentNumberSecond, setCurrentNumberSecond] = useState("");
+  const [operator, setOperator] = useState("");
+  // let currentNumberSecond;
+  // let currentNumberFirst = 0;
+  const [res, setRes] = useState("");
+
+  const onClickBtn = (event) => {
+    if (operator === "") {
+      setCurrentNumberFirst(currentNumberFirst + event.target.innerText);
+    } else {
+      setCurrentNumberSecond(currentNumberSecond + event.target.innerText);
+    }
+  };
+
+  const onClickPluse = () => {
+    setOperator("+");
+    if (res !== "") {
+      setCurrentNumberFirst(res);
+      setRes("");
+      setCurrentNumberSecond("");
+    }
+  };
+
+  const onClickMinus = () => {
+    setOperator("-");
+    if (res !== "") {
+      setCurrentNumberFirst(res);
+      setRes("");
+      setCurrentNumberSecond("");
+    }
+  };
+
+  const onClickResult = () => {
+    if (operator === "+") {
+      setRes(Number(currentNumberFirst) + Number(currentNumberSecond));
+    } else if (operator === "-") {
+      setRes(Number(currentNumberFirst) - Number(currentNumberSecond));
+    }
+  };
+
+  const onClickReset = () => {
+    setRes("");
+    setCurrentNumberFirst("");
+    setCurrentNumberSecond("");
+    setOperator("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"></img>
+    <div className={styles.container}>
+      <div className={styles.calculator}>
+        <div className={res !== "" ? styles.displayRes : styles.display}>
+          {" "}
+          {res !== ""
+            ? res
+            : currentNumberFirst + operator + currentNumberSecond}
+        </div>
+        {btnNumbers.map(({ id, name }) => (
+          <button onClick={onClickBtn} key={id}>
+            {name}
+          </button>
+        ))}
 
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button onClick={onClickPluse}>+</button>
+        <button onClick={onClickMinus}>-</button>
+        <button onClick={onClickResult}>=</button>
+        <button className={styles.resetBtn} onClick={onClickReset}>
+          C
+        </button>
+      </div>
     </div>
   );
 }
