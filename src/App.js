7a8a7ce308b5e1c136dbdfd3071cdd635e2e07fc
Anastasiya <anastasiya.ibr@mail.ru>
2023-11-28 22:00:33 +0300

    useRef

--- src/App.js
+++ src/App.js
@@ -1,133 +1,27 @@
-import "./App.css";
-import { useState } from "react";
-
-/*---------------------1 способ-------------------------------*/
-// function App() {
-//   const onSubmit = (event) => {
-//     event.preventDefault();
-//     console.log({ login });
-//   };
-
-//   const [login, setLogin] = useState("");
-//   const [loginError, setLoginError] = useState(null);
-
-//   const onLoginChange = ({ target }) => {
-//     setLogin(target.value);
-
-//     let newError = null;
-//     if (!/^[\w_]*$/.test(target.value)) {
-//       newError =
-//         "Неверный логин. Допустимые символы: буквы, цифры и нижнее подчёркивание";
-//     } else if (target.value.length > 20) {
-//       newError = "Неверный логин. Должно быть не больше 20 символов";
-//     }
-//     setLoginError(newError);
-//   };
-
-//   const onLoginBlur = ({ target }) => {
-//     if (target.value.length < 3) {
-//       setLoginError("Неверный логин. Должно быть не меньше 3 символов");
-//     }
-//   };
-
-//   return (
-//     <div className="App">
-//       <form onSubmit={onSubmit}>
-//         {loginError && <div>{loginError}</div>}
-//         <input
-//           type="text"
-//           name="login"
-//           placeholder="логин"
-//           value={login}
-//           onChange={onLoginChange}
-//           onBlur={onLoginBlur}
-//         />
-//         <button type="submit" disabled={!!loginError}>
-//           Отправить
-//         </button>
-//       </form>
-//     </div>
-//   );
-// }
-// export default App;
-
-/*--------------------2 способ с помощью пакета yap----------------------------*/
-import * as yup from "yup";
-import { useRef } from "react";
-
-//описываем схему для валидации для кажого обработчика
-const loginChangeSchema = yup
-  .string()
-  .matches(
-    /^[\w_]*$/,
-    "Неверный логин. Допустимые символы: буквы, цифры и нижнее подчёркивание",
-  )
-  .max(20, "Неверный логин. Должно быть не больше 20 символов");
-
-const loginBlurSchema = yup
-  .string()
-  .min(3, "Неверный логин. Должно быть не меньше 3 символов");
-
-//функция, которая занимается валидацией с помощью описанных выше схем:
-const validateAndGetErrorMessage = (schema, value) => {
-  let errorMessage = null;
-
-  try {
-    schema.validateSync(value, { abortEarly: false });
-  } catch ({ errors }) {
-    errorMessage = errors.join("\n");
-  }
-
-  return errorMessage;
-};
+import styles from "./App.module.css";
+import { useState, useRef } from "react";
 
 function App() {
-  const [login, setLogin] = useState("");
-  const [loginError, setLoginError] = useState(null);
+  const [stateCount, setStateCount] = useState(0);
+  const refCount = useRef(0);
 
-  const submitBattonRef = useRef(null);
-
-  const onLoginChange = ({ target }) => {
-    setLogin(target.value);
-    const newError = validateAndGetErrorMessage(
-      loginChangeSchema,
-      target.value,
-    );
-    setLoginError(newError);
-
-    if (target.value.length === 20) {
-      submitBattonRef.current.focus();
-    }
+  const onClickRefCount = () => {
+    refCount.current += 1;
+    console.log("refCount = " + refCount.current);
   };
 
-  const onLoginBlur = () => {
-    const error = validateAndGetErrorMessage(loginBlurSchema, login);
-    setLoginError(error);
-  };
-
-  const onSubmit = (event) => {
-    event.preventDefault();
-    console.log({ login });
+  const onClickStateCount = () => {
+    setStateCount(stateCount + 1);
+    console.log("stateCount = " + stateCount + 1);
   };
 
   return (
-    <div className="App">
-      <form onSubmit={onSubmit}>
-        {loginError && <div>{loginError}</div>}
-        <input
-          type="text"
-          name="login"
-          placeholder="логин"
-          value={login}
-          onChange={onLoginChange}
-          onBlur={onLoginBlur}
-        />
-        <button type="submit" disabled={!!loginError} ref={submitBattonRef}>
-          Отправить
-        </button>
-      </form>
+    <div className={styles.App}>
+      <p>refCount: {refCount.current}</p>
+      <button onClick={onClickRefCount}>Прибавить к refCount</button>
+      <p>stateCount: {stateCount}</p>
+      <button onClick={onClickStateCount}>Прибавить к stateCount</button>
     </div>
   );
 }
-
 export default App;
