2a5f2d9ac3f0af700f73a5855a8ec3fa0ebbbbf5
Anastasiya <anastasiya.ibr@mail.ru>
2024-01-26 22:48:05 +0300

    tailwine css

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,11 @@
 import styles from "./App.module.css";
 
-export const WithLoggining = (Component) => {
-	const NewComponent = (props) => {
-		console.log(props.user);
-
-		return <Component {...props} />;
-	};
-
-	return NewComponent;
-};
-
-export const WithLogginingAndColor = (Component, color) => {
-	const NewComponent = (props) => {
-		console.log(props.user);
-
-		return (
-			<span style={{ color }}>
-				<Component {...props} />
-			</span>
-		);
-	};
-
-	return NewComponent;
-};
-
-export const HelloMessage = ({ user }) => {
-	return <span>Привет {user}</span>;
-};
-
-// export const GoodbyeMessage = ({ user }) => {
-// 	return <span>До свидания {user}</span>;
-// };
-
-const HelloMessageWithLoggin = WithLoggining(HelloMessage);
-const HelloMessageWithLogginAndColorRed = WithLogginingAndColor(
-	HelloMessage,
-	"red",
-);
-
-export const UserWidget = ({ Message }) => {
-	const user = "Maxim";
-
-	return (
-		<div>
-			<div>Текущий пользователь: {user}</div>
-			<div>Сообщение:</div>
-			<HelloMessageWithLoggin user={user} />
-			<HelloMessageWithLogginAndColorRed user={user} />
-			{/* <Message user={user} /> */}
-		</div>
-	);
-};
-
 export const App = () => {
 	return (
-		<div className={styles.app}>
-			<UserWidget />
-			{/* <UserWidget Message={HelloMessage} />
-			<UserWidget Message={GoodbyeMessage} /> */}
+		<div className="text-center text-emerald-600 text-xl">
+			<div>Контент</div>
+			<div className="text-green-400 hover:text-blue-600">Anastasi</div>
+			<div className="circle-yellow"></div>
 		</div>
 	);
 };
